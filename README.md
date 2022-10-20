90ce06301d569b0820ca211e0a1382c3a1f6af15
Rajat Sharma <lunasunkaiser@gmail.com>
2022-10-21 03:13:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# IFSC-CLI
+
+> Simple cli to get Bank information from IFSC (Indian Financial System Code)
+
+## Get Started
+
+### Install locally
+
+```shell
+cargo install --git https://github.com/rajatsharma/ifsc-cli
+```
+
+## Usage
+
+```sh
+ifsc [BANKIFSC]
+```
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::{env, process::exit};
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Bank {
+    #[serde(rename(deserialize = "BANK"))]
+    bank: String,
+    #[serde(rename(deserialize = "BRANCH"))]
+    branch: String,
+    #[serde(rename(deserialize = "ADDRESS"))]
+    address: String,
+    #[serde(rename(deserialize = "CITY"))]
+    city: String,
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.get(1).is_none() {
+        println!("no arg passed, need at least one, PROJECT_NAME");
+        exit(1)
+    }
+
+    let response = reqwest::blocking::get(format!(
+        "https://ifsc.razorpay.com/{}",
+        args.get(1).unwrap()
+    ));
+
+    let bank = response
+        .and_then(|res| res.json::<Bank>())
+        .expect("unable to parse response");
+
+    println!("BANK: {}", bank.bank);
+    println!("BRANCH: {}", bank.branch);
+    println!("ADDRESS: {}", bank.address);
+    println!("CITY: {}", bank.city);
+}
